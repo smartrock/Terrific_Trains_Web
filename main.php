ff44da9f49300f224edc634f9fd21a3326665f5e
smartrock <107009802+smartrock@users.noreply.github.com>
2023-02-13 13:18:12 +1300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>Terrific Trains</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="Terrific Trains" content="trains, steam trains, diesel trains, electric trains, train history, type of trians">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style-main.css" media ="screen">
+        <link rel="icon" href="images/icon-logo.png">
+    </head>
+    <body>
+        <div class = "grid-container">
+            <nav>
+                <ul>
+                    <li><a href="main.php">Home</a></li>					
+			        <li><a href="browse.php">Browse</a></li> 
+			        <li><a href="history.php">History</a></li> 
+			        <li><a href="principles.php">Principles</a></li> 
+                    <li><a href="location.php">Location</a></li>
+		        </ul>
+            </nav>
+
+            <article class = "left_column">
+                <p>
+                    Superior Steam
+                </p>
+            </article>
+
+            <article class = "mid_column">
+                <p>
+                    Superior Steam
+                </p>
+            </article>
+        
+            <script src="" async defer></script>
+
+            <footer>
+
+            </footer>
+        </div>
+    </body>
+</html>
